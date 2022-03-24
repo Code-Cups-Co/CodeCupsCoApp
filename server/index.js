bac24360a535938b0b9356912878d8cc92d2ade4
Joel Deon Dsouza <oka.welt.dev@gmail.com>
2022-03-24 14:29:47 +0100

    Server connected to DB

--- server/index.js
+++ server/index.js
@@ -2,3 +2,26 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+// const dotenv = require("dotenv");
+
+// dotenv.config();
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+//Connect dataBase//
+
+const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect(MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(
+        `Server Connected to DB and running on Port: http://localhost:${PORT}`
+      )
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
